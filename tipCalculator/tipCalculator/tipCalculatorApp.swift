c68f24adfa9c610e55c7e792cad9ac77fa9be468
Daniel Ramirez <daniel.ramirezm@ingenieria.unam.edu>
2026-03-07 22:04:52 -0600

    first commit

---
+++ tipCalculator/tipCalculator/tipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipCalculatorApp.swift
+//  tipCalculator
+//
+//  Created by Daniel Ramirez on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct tipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
